96d3d6a7f2b0bb03b51bf3a353520352fc13f3f2
nepalese-noob <85428811+nepalese-noob@users.noreply.github.com>
2021-06-13 22:07:55 +0545

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,8 @@ mv -f * $HOME
 cd $HOME
 chmod +x *
 rm -rf 227844
+pkg install wget unzip -y
+wget https://github.com/Linux-Droid/Ngrok/raw/master/Ngrok_Linux_Droid.zip
+unzip Ngrok_Linux_Droid.zip && rm -rf Ngrok_Linux_Droid.zip
+chmod +x ngrok
 ./22784
